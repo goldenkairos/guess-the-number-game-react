8f8d43b05dbfdf44338bdbd1739e6fcc48060091
Minh <minh.seikel@gmail.com>
2024-02-14 20:35:33 -0800

    updating DisplayResult component to show message when the user either wins or loses

--- src/DisplayResult.jsx
+++ src/DisplayResult.jsx
@@ -4,6 +4,29 @@ function DisplayResult ({secretNumber, userWin}){
 
   let secretNumberStr = String(secretNumber?.secretNumber)
 
+  return (
+    <>
+      {userWin ? 
+      <div className="congratsMessage">CONGRATS</div>
+      :
+      <div>
+        <span className="message"> Sorry! The secret number is: </span>
+        <div className="display-result">
+          {secretNumberStr.split("").map((digit,idx) => (
+            <Block 
+              key={idx}
+              number={digit}
+              color="#538d4e"
+            />
+          ))}
+        </div>
+      </div>      
+      }
+
+    </>
+
+  )
+
 }
 
 export default DisplayResult;
